299d408708e392cf9ab72e21e30bba987136dda8
grib-satanist <zastenchivyi.grib.satanist@gmail.com>
2020-04-11 20:51:44 +0500

    Переделать структуру для filter dropdown
    Добавить стилей + скрипт для работы с датой (для filter dropdown)

--- src/blocks/form_elements/data_dropdown/data_dropdown.js
+++ src/blocks/form_elements/data_dropdown/data_dropdown.js
@@ -1,8 +1,32 @@
 // let _ = require('../../../../node_modules/datepicker/jquery.datepicker.extension.range.min');
 let _ = require('air-datepicker');
 
+/** Для фильтра дат */
 
-
+$('#filter_dropdown').datepicker({ 
+  clearButton:true,
+  dateFormat: "d M",
+ //  inline: true, /* Делает календарь видимым всегда*/
+  navTitles: {
+   days: 'MM yyyy',
+   months: 'yyyy',
+   years: 'yyyy1 - yyyy2'
+   },
+  onShow(inst, animationCompleted) {
+     if (!animationCompleted) {
+       if (!inst.$datepicker.find('.datepicker--buttons .datepicker--button-apply').length) {
+         $('<span class="datepicker--button datepicker--button-apply">Применить</span>').on('click', function (e) {
+           e.stopPropagation();
+           inst.hide();
+         }).appendTo(inst.$datepicker.find('.datepicker--buttons'));
+       }
+     }
+     
+   },
+   nextHtml: '<span class="material-icons datapicker_arrow">arrow_forward</span>',
+   prevHtml: '<span class="material-icons datapicker_arrow">arrow_back</span>',
+   
+});
 
 /** Для диапазона дат с двумя input */
 
@@ -15,8 +39,8 @@ $('#range_start').datepicker({
     years: 'yyyy1 - yyyy2'
     },
    onSelect: function (fd, d, picker) { 
-     $("#range_start").val(fd.split(",")[0]);
-     $("#range_end").val(fd.split(",")[1]);
+     $("#range_start").val(fd.split("-")[0]);
+     $("#range_end").val(fd.split("-")[1]);
     
    },
    onShow(inst, animationCompleted) {
@@ -33,11 +57,9 @@ $('#range_start').datepicker({
     nextHtml: '<span class="material-icons datapicker_arrow">arrow_forward</span>',
     prevHtml: '<span class="material-icons datapicker_arrow">arrow_back</span>',
     showDatapicker: $('#range_end').on('click', function (ev) {
-      var myDatepicker = $('#range_start').datepicker().data('datepicker');
+      var myDatepicker = $('#range_start').data('datepicker');
 
       myDatepicker.show();
     })
     
  });
-
- 
