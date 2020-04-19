25633440619e13352118856c466b0e2cec61a9d3
grib-satanist <zastenchivyi.grib.satanist@gmail.com>
2020-04-19 20:13:16 +0500

    Добавить карточку с регистрацией пользователя и стилизовать её
    - обернуть все вводимые данные с карточек в form со своим id для будующего взаимодействия
    - небольшие  правки стилей для buttons, text_field, toggle

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import "./ui_kit.styl";
 import "./blocks/form_elements/form_elements";
 import "./blocks/cards/cards";
 
-    $('.mask-date').mask('31.12.9999');
+    $('.mask-date').mask('99.99.9999');
    
