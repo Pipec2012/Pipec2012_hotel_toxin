ec8cbfc136291bf60de41177c083523c46cc3b7a
grib-satanist <zastenchivyi.grib.satanist@gmail.com>
2020-04-22 00:20:01 +0500

    Переписать mixin элемента rate_button
    1. Теперь для rate_button можно задавать как количество звёзд, так и количество активных по умолчанию.
    2.  Написать mixin для карточки с номерами
    + Добавить js для слайдера в карточку с номерами (room)

--- src/blocks/cards/room/room.js
+++ src/blocks/cards/room/room.js
@@ -1,1 +1,12 @@
 let _ = require('slick-carousel');
+
+   $(document).ready(function(){
+      $('.room_photo__slider').slick({
+         // dots: true,
+         infinite: true,
+         slidesToShow: 1,
+         
+         nextArrow: '<span class="material-icons room_slider__next">keyboard_arrow_right</span>',
+         prevArrow: '<span class="material-icons room_slider__prev">keyboard_arrow_left</span>',
+      });
+   });
