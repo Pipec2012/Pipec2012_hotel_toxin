de840703a5ed2c2036a727e2dc1e9c9b022b5ee2
grib-satanist <zastenchivyi.grib.satanist@gmail.com>
2020-11-04 14:27:43 +0500

    сделать адаптивной страницу поиска номеров
    - добавлена скрывающаяся панель фильтров на странице поиска номеров

--- src/blocks/headers_footers/headers/header.js
+++ src/blocks/headers_footers/headers/header.js
@@ -5,13 +5,14 @@ const burgerMenu = document.querySelector('.header .btn_burger-menu'),
    mobileMenu = document.querySelector('.header_mobile_navbar__block'),
    loginBlock = document.querySelector('.header .navbar_login__block');
    
-
-burgerMenu.addEventListener('click', () => {
-   mobileMenu.classList.toggle('display-none');
-   loginBlock.classList.toggle('display-none');
-} )
-
-closeButton.addEventListener('click', () => {
-   mobileMenu.classList.toggle('display-none');
-   loginBlock.classList.toggle('display-none');
-} )
+if (burgerMenu) {
+   burgerMenu.addEventListener('click', () => {
+      mobileMenu.classList.toggle('display-none');
+      loginBlock.classList.toggle('display-none');
+   } )
+   
+   closeButton.addEventListener('click', () => {
+      mobileMenu.classList.toggle('display-none');
+      loginBlock.classList.toggle('display-none');
+   } )
+}
--- src/index.js
+++ src/index.js
@@ -4,6 +4,19 @@ import "./ui_kit.styl";
 import "./blocks/form_elements/form_elements";
 import "./blocks/cards/cards";
 import "./blocks/headers_footers/headers/header";
+import { event } from "jquery";
 
     $('.mask-date').mask('99.99.9999');
    
+
+var roomSearchFilterBnt = document.querySelector('.rooms_mobile__btn'),
+    roomSearchFilterClose = document.querySelector('.mobile_filter_close'),
+    filterBlock = document.querySelector('.filter_column');
+
+roomSearchFilterBnt.addEventListener('click', () => {
+    filterBlock.classList.remove('mobile_filter_slide');
+})
+
+roomSearchFilterClose.addEventListener('click', () => {
+    filterBlock.classList.add('mobile_filter_slide');
+})
